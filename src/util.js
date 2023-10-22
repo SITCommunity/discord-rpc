95e8bce94c8b1066b6a3816fc6a0f60f3f41d509
BrokenEdtz <jesenfu1001@gmail.com>
2023-10-22 11:33:02 +0700

    fix Insecure randomness #2 high severity

--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const RpcError = require('../lib/error/RpcError');
+
 let register;
 try {
   const { app } = require('electron');
@@ -7,39 +9,40 @@ try {
 } catch (err) {
   try {
     register = require('register-scheme');
-  } catch (e) {} // eslint-disable-line no-empty
-}
+  } catch (e) {
+    throw new RpcError(); // eslint-disable-line no-empty
+  };
+};
 
 if (typeof register !== 'function') {
   register = () => false;
-}
+};
 
 function pid() {
   if (typeof process !== 'undefined') {
     return process.pid;
-  }
+  };
   return null;
-}
+};
 
 const uuid4122 = () => {
+  const crypto = window.crypto || window.msCrypto; // support for cross-browser
+  if (!crypto || !crypto.getRandomValues) {
+    throw new RpcError('This Crypto API not support in this browser');
+  };
+
+  const randomValues = new Uint8Array(16);
+  crypto.getRandomValues(randomValues);
+
+  randomValues[6] = (randomValues[6] & 0x0f) | 0x40;
+  randomValues[8] = (randomValues[8] & 0x3f) | 0x80;
   let uuid = '';
-  for (let i = 0; i < 32; i += 1) {
-    if (i === 8 || i === 12 || i === 16 || i === 20) {
+  for (let i = 0; i < 16; i++) {
+    if (i === 4 || i === 6 || i === 8 || i === 10) {
       uuid += '-';
-    }
-    let n;
-    if (i === 12) {
-      n = 4;
-    } else {
-      const random = Math.random() * 16 | 0;
-      if (i === 16) {
-        n = (random & 3) | 0;
-      } else {
-        n = random;
-      }
-    }
-    uuid += n.toString(16);
-  }
+    };
+    uuid += randomValues[i].toString(16).padStart(2, '0');
+  };
   return uuid;
 };
 
